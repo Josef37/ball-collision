c6f5349678b173c8c990557db536fbce88390ebc
Josef Wittmann <josef.wittmann@tutanota.com>
2020-12-26 18:54:56 +0100

    Refactor position in collision

--- src/index.js
+++ src/index.js
@@ -41,6 +41,14 @@ class Ball {
         this.x += x
         this.y += y
     }
+    setPosition({ x, y }) {
+        this.x = x
+        this.y = y
+    }
+    setVelocity({ vx, vy }) {
+        this.vx = vx
+        this.vy = vy
+    }
     get kineticEnergy() {
         return 1 / 2 * this.mass * this.velocity
     }
@@ -89,15 +97,17 @@ const collideTwoBalls = (ball0, ball1, dt) => {
 
     const { dx, dy } = getDistanceVector(ball0, ball1);
 
-    //calculate angle, sine, and cosine
+
+
+    // calculate angle, sine, and cosine
     const angle = Math.atan2(dy, dx);
     const sin = Math.sin(angle);
     const cos = Math.cos(angle);
 
-    // For the computation, rotate and translate the two ball, 
+    // For the computation, rotate and translate the two balls, 
     // that ball0 is at (0,0) and ball1 is at (dist,0)
-    const pos0 = { x: 0, y: 0 };
-    const pos1 = rotateClockwise(dx, dy, sin, cos);
+    let pos0 = 0;
+    let pos1 = rotateClockwise(dx, dy, sin, cos).x;
 
     const vel0 = rotateClockwise(ball0.vx, ball0.vy, sin, cos);
     const vel1 = rotateClockwise(ball1.vx, ball1.vy, sin, cos);
@@ -111,28 +121,36 @@ const collideTwoBalls = (ball0, ball1, dt) => {
 
     // Undo overlap according to new velocities
     const absV = Math.abs(vel0.x) + Math.abs(vel1.x);
-    const overlap = ball0.radius + ball1.radius - Math.abs(pos0.x - pos1.x);
-    pos0.x += (vel0.x / absV) * overlap;
-    pos1.x += (vel1.x / absV) * overlap;
+    const overlap = ball0.radius + ball1.radius - Math.abs(pos0 - pos1);
+    pos0 += (vel0.x / absV) * overlap;
+    pos1 += (vel1.x / absV) * overlap;
 
     //rotate positions back
-    const pos0F = rotateCounterClockwise(pos0.x, pos0.y, sin, cos);
-    const pos1F = rotateCounterClockwise(pos1.x, pos1.y, sin, cos);
+    const pos0F = rotateCounterClockwise(pos0, 0, sin, cos);
+    const pos1F = rotateCounterClockwise(pos1, 0, sin, cos);
 
     //adjust positions to actual screen positions
-    ball1.x = ball0.x + pos1F.x;
-    ball1.y = ball0.y + pos1F.y;
-    ball0.x = ball0.x + pos0F.x;
-    ball0.y = ball0.y + pos0F.y;
+    ball1.setPosition({
+        x: ball0.x + pos1F.x,
+        y: ball0.y + pos1F.y
+    })
+    ball0.setPosition({
+        x: ball0.x + pos0F.x,
+        y: ball0.y + pos0F.y
+    })
 
     //rotate velocities back
     const vel0F = rotateCounterClockwise(vel0.x, vel0.y, sin, cos);
     const vel1F = rotateCounterClockwise(vel1.x, vel1.y, sin, cos);
 
-    ball0.vx = vel0F.x;
-    ball0.vy = vel0F.y;
-    ball1.vx = vel1F.x;
-    ball1.vy = vel1F.y;
+    ball0.setVelocity({
+        vx: vel0F.x,
+        vy: vel0F.y
+    })
+    ball1.setVelocity({
+        vx: vel1F.x,
+        vy: vel1F.y
+    })
 };
 
 function isNoCollision(ball0, ball1) {
